7edb7c4a38b7a541f16dab9710f13bd8db9347a4
Klaudia Błażyczek <blazyczek.klaudia@gmail.com>
2022-05-24 13:19:40 +0200

    Cleanup

--- data_reader.py
+++ data_reader.py
@@ -1,13 +1,19 @@
 import json
 from copy import deepcopy
+import pandas as pd
+import csv
 
 
-# reads data from file, transforms and saves to csv file
-# 9 records are skipped, because they contain unwanted signs
+# reads data from file, transforms and saves to csv file - merges all helper methods into one
+from ctypes import Union
+
+
 def process_data_and_write_to_csv():
     raw_data = read_data()
     data = []
+
     print('Initial amount of lines: ' + str(len(raw_data)))
+
     for index in range(len(raw_data)):
         try:
             data.append(utf_16_encoding_to_8_fix(raw_data[index]))
@@ -16,16 +22,16 @@ def process_data_and_write_to_csv():
 
     print('Amount of lines after deleting lines with bad format: ' + str(len(data)))
 
-    data = data_transformer(data)
-    genres_count = count_genres(data)
-    data = filter_genres(data, genres_count)
+    a = transform_data_to_dict(data)
+    b = remove_to_wide_or_to_specific_genres(a)
+    c = remove_books_with_too_many_genres(b)
+    d = transform_temporary_dictionary_to_target_dictionary(c)
+    save_data_to_csv(d)
 
-    genres_count = count_genres(data)
-    sorted_counts = sorted(genres_count.items(), key=lambda x: x[1], reverse=True)
-    for item in sorted_counts:
-        print(item[0] + " " + str(item[1]))
+    # for key in data.keys():
+    #     print("\t" + str(key) + " " + str(data[key]))
 
-    print('Amount of lines after deleting lines with no genres: ' + str(len(data)))
+    print('Amount of lines after cleaning: ' + str(len(data)))
 
 
 # reads data from file and returns as list of lines - strings
@@ -49,34 +55,36 @@ def utf_16_encoding_to_8_fix(data: str) -> str:
     return ''.join(data)
 
 
-# transforms data to dict: key - Wiki ID, value - dict with: title, genres and summary
-def data_transformer(data: list[str]) -> dict[int, dict[str, list[str]]]:
+# transforms data to dict where: key - Wiki ID, value - dict with: title, genres and summary
+def transform_data_to_dict(data: list[str]) -> dict[int, dict[str, list[str]]]:
     temporary_data = {}
     for line in data:
         line = line.split("\t")
 
         # deletes leading and trailing whitespaces from elements that will be used
+        # line[0] - ID from Wiki, line[2] - title, line[5] - genres, line[6] - summary
+        line[0] = line[0].strip()
         line[2] = line[2].strip()
         line[5] = line[5].strip()
         line[6] = line[6].strip()
 
-        # delete elements that doesnt have Wiki ID, title, genres or summary
+        # delete elements that doesnt have Wiki ID, title, genres, summary or have bad content
         if line[0] != '' and line[2] != '' and line[5] != '' and line[6] != '' and line[6] != 'To be added.' and\
                 line[6].count('=') == 0 and line[6].count("\\") == 0 and line[6].count('/') == 0 and \
                 line[6].count('<!') == 0 and line[6].count('<') == 0 and line[6].count('&nbsp') == 0 and \
                 line[6].count('&mdash') == 0 and line[6].count('&ndash') == 0 and line[6].count('&#') == 0 and \
                 line[6].count('http') == 0:
-            # line[0] - ID from Wiki, line[2] - title, line[5] - genres, line[6] - summary
-            # temporary_data[int(line[0])] = [line[index] for index in (2, 5, 6)]
-            nested_dict = {'title': line[2], 'summary': line[6],
-                           'genres': [genre for genre in json.loads(line[5]).values()]}
-            temporary_data[int(line[0])] = nested_dict
 
-    print('Amount of lines after deleting lines with wrong data: ' + str(len(temporary_data)))
+            nested_dict = {'title': [line[2]],
+                           'summary': [line[6]],
+                           'genres': [genre for genre in json.loads(line[5]).values()]}
+
+            temporary_data[int(line[0])] = nested_dict
 
     return temporary_data
 
 
+# counts genres occurrences among books
 def count_genres(temp_dict: dict[int, dict[str, list[str]]]) -> dict[str, int]:
     genre_count = {}
     for element in temp_dict.values():
@@ -89,11 +97,14 @@ def count_genres(temp_dict: dict[int, dict[str, list[str]]]) -> dict[str, int]:
     return genre_count
 
 
-# deletes genres with too wide or too specific genres
-def filter_genres(temp_dict: dict[int, dict[str, list[str]]], genre_count: dict[str, int]) -> dict[int, dict[str, list[str]]]:
+# deletes genres with too wide or too specific genres from book genres
+# also deletes books with no genres left
+def remove_to_wide_or_to_specific_genres(temp_dict: dict[int, dict[str, list[str]]]) -> dict[int, dict[str, list[str]]]:
+    genres_count = count_genres(temp_dict)
+
     for book in temp_dict.values():
         for genre in deepcopy(book['genres']):
-            if genre_count[genre] < 500 or genre_count[genre] > 3000:
+            if genres_count[genre] < 1000 or genres_count[genre] > 3000:
                 book['genres'].remove(genre)
 
     tmp = deepcopy(temp_dict)
@@ -103,3 +114,64 @@ def filter_genres(temp_dict: dict[int, dict[str, list[str]]], genre_count: dict[
 
     return temp_dict
 
+
+# Remove books that have more than one genre from the dictionary
+def remove_books_with_too_many_genres(temp_dict: dict[int, dict[str, list[str]]]) -> dict[int, dict[str, list[str]]]:
+    genres_count = count_genres(temp_dict)
+    tmp = deepcopy(temp_dict)
+
+    for key in tmp:
+        genres_of_book = set(temp_dict[key]['genres'])
+        intersection = genres_of_book.intersection(genres_count.keys())
+
+        if len(list(intersection)) > 1:
+            temp_dict.pop(key)
+
+    # TODO - zapytać Jakubika czy ok, a jak nie to czy sb można wyjebać, żeby były podobne liczby
+    # counts = count_genres(temp_dict)
+    # sorted_counts = sorted(counts.items(), key=lambda x: x[1], reverse=True)
+    # print("Genre list with counts: ")
+    # for item in sorted_counts:
+    #     print("\t" + item[0] + " " + str(item[1]))
+
+    return temp_dict
+
+
+# transforms temporary dictionary to target dictionary
+def transform_temporary_dictionary_to_target_dictionary(temp_dict: dict[int, dict[str, list[str]]]) -> dict[int, dict[str, str]]:
+    final_dict = {}
+
+    for key in temp_dict.keys():
+        final_dict[key] = {'title': temp_dict[key]['title'][0],
+                           'genre': temp_dict[key]['genres'][0],
+                           'summary': temp_dict[key]['summary'][0]}
+
+    return final_dict
+
+
+# saves cleaned data to csv file
+def save_data_to_csv(data: dict[int, dict[str, str]]):
+    data_to_save = []
+
+    for key in data:
+        data_to_save.append([str(key), data[key]['title'], data[key]['genre'], data[key]['summary']])
+
+    with open("cleaned_data.csv", "w", encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerows(data_to_save)
+
+
+# reads cleaned data from csv and saves to dictionary
+def read_data_from_csv() -> dict[int, dict[str, str]]:
+    df = pd.read_csv('cleaned_data.csv', header=None)
+    data = {}
+
+    # indexes: 0 - Wiki ID, 1, title, 2 - genre, 3 - summary
+    for line_index in df.index:
+        data[int(df.loc[line_index, 0])] = {
+            'title': str(df.loc[line_index, 1]),
+            'genre': str(df.loc[line_index, 2]),
+            'summary': str(df.loc[line_index, 3])
+        }
+
+    return data
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from data_reader import process_data_and_write_to_csv
+from data_reader import process_data_and_write_to_csv, read_data_from_csv
 
 if __name__ == '__main__':
-    process_data_and_write_to_csv()
-
+    # process_data_and_write_to_csv()
+    read_data_from_csv()
