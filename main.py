eb3d0c089b5beed8aa064ad05c1ebb2fcc245843
Klaudia Błażyczek <blazyczek.klaudia@gmail.com>
2022-05-31 19:23:02 +0200

    Remove data from main

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from data_reader import process_data_and_write_to_csv, read_data_from_csv_to_dic
 from data_analyzer import analyze_data
 
 if __name__ == '__main__':
-    process_data_and_write_to_csv()
+    # process_data_and_write_to_csv()
     # data = read_data_from_csv_to_dict()
     # analyze_data(data)
+    pass
+
+
