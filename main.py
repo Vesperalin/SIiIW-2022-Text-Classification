29d00e87c7bc184acf3bc366c27be1dadbf968b7
Klaudia Blazyczek <blazyczek.klaudia@gmail.com>
2022-06-05 11:48:24 +0200

    experiments

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from data_reader import process_data_and_write_to_csv, read_data_from_csv_to_dict
 from data_analyzer import analyze_data
-from ml_system import bayes_tuning, svm_tuning, cross_validation_naive_bayes
+from ml_system import bayes_tuning, svm_tuning, cross_validation_naive_bayes, cross_validation_svm
 
 if __name__ == '__main__':
     # process_data_and_write_to_csv()
     # data = read_data_from_csv_to_dict()
     # analyze_data(data)
-    # cross_validation_naive_bayes(True, )
-    bayes_tuning()
+    # cross_validation_naive_bayes(True, 0.5, 0.001, 5000, (1, 2), 0.1)
+    cross_validation_svm(True, 0.5, 0.001, 5000, (1, 1), 0.1, 'squared_hinge')
+    # bayes_tuning()
     # svm_tuning()
     pass
 
